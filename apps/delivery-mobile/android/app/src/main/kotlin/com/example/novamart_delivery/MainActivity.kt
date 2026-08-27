b1072472aea929c996c0a00b48197c34d3f9c51e
Ankit Raj <ankitraj667346@gmail.com>
2026-08-27 10:36:56 +0530

    build: add Android projects for mobile apps

---
+++ apps/delivery-mobile/android/app/src/main/kotlin/com/example/novamart_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.novamart_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/warehouse-mobile/android/app/src/main/kotlin/com/example/novamart_warehouse/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.novamart_warehouse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
